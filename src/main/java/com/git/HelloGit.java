ca3e2ae5994a796330186ebbcc097dae9fb18029
lgl-nb <2667089090@qq.com>
2021-06-20 00:47:21 +0800

    项目提交

---
+++ src/main/java/com/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author lglstart
+ * @create 2021-06-20 0:18
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
